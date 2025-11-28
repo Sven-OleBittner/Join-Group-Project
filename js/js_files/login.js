73965f04bab83e70e74f8f15393f2d2122a8961c
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-11-28 18:45:56 +0100

    fix: standardize quotes and clean up comments in login.js

--- js/js_files/login.js
+++ js/js_files/login.js
@@ -17,24 +17,21 @@ function login() {
     (u) => u.email == email.value && u.password == password.value
   );
   if (user) {
-    window.location.href = 'summary.html'
+    window.location.href = "summary.html";
   } else {
     console.log("user nicht registriert");
   }
 }
 
 function showMsg() {
-  // Source - https://stackoverflow.com/a
-  // Posted by Artem Barger, modified by community. See post 'Timeline' for change history
-  // Retrieved 2025-11-23, License - CC BY-SA 4.0
-    let msgBox = document.getElementById("msgBox");
-    let msgBoxText = document.getElementById("msgBoxText");
+  let msgBox = document.getElementById("msgBox");
+  let msgBoxText = document.getElementById("msgBoxText");
   const urlParams = new URLSearchParams(window.location.search);
   const msg = urlParams.get("msg");
   if (msg) {
     msgBoxText.innerHTML = msg;
   } else {
-    msgBoxText.style.display = 'none';
-    msgBox.style.display = 'none';
+    msgBoxText.style.display = "none";
+    msgBox.style.display = "none";
   }
 }
