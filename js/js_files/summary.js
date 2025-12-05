74a59c073953910af5cc52bac86df27a3322246d
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-12-05 19:11:00 +0100

    feat: add greeting box element to display personalized user greeting

--- js/js_files/summary.js
+++ js/js_files/summary.js
@@ -16,6 +16,7 @@ async function loadUserData() {
 function userFound(logInUser) {
   let userInitials = document.getElementById("userInitials");
   let greetingText = document.getElementById("greetingText");
+  let greetingBox = document.getElementById('greetingBox');
   const nameWords = logInUser.name.split(" ");
   const initials = nameWords
     .map((word) => word.charAt(0).toUpperCase())
@@ -23,4 +24,5 @@ function userFound(logInUser) {
     .substring(0, 2);
   userInitials.innerHTML = initials;
   greetingText.innerHTML = `Good Morning ${logInUser.name} !`;
+  greetingBox.innerHTML = `Good Morning ${logInUser.name} !`;
 }
