b38d975ceaac6bcc556e3628b5b11d6961669605
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-12-09 16:37:26 +0100

    fix: change the id, class and color for the greeting box

--- js/js_files/summary.js
+++ js/js_files/summary.js
@@ -16,14 +16,14 @@ async function loadUserData() {
 
 function userFound(logInUser) {
   let userInitials = document.getElementById("userInitials");
-  let greetingText = document.getElementById("greetingText");
-  let greetingBox = document.getElementById("greetingBox");
+  let greetingNameResp = document.getElementById("greetingNameResp");
+  let greetingName = document.getElementById("greetingName");
   const nameWords = logInUser.name.split(" ");
   const initials = nameWords
     .map((word) => word.charAt(0).toUpperCase())
     .join("")
     .substring(0, 2);
   userInitials.innerHTML = initials;
-  greetingText.innerHTML = `Good Morning ${logInUser.name} !`;
-  greetingBox.innerHTML = `Good Morning ${logInUser.name} !`;
+  greetingNameResp.innerHTML = logInUser.name;
+  greetingName.innerHTML = logInUser.name;
 }
