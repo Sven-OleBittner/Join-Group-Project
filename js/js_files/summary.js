241470fb208b4d99b15b7ddc2ef4aaaf59adb9a9
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-12-21 15:49:05 +0100

    feat: enhance greeting functionality and task data loading in summary

--- js/js_files/summary.js
+++ js/js_files/summary.js
@@ -1,5 +1,7 @@
 function initSummary() {
   loadUserData();
+  greetingBasedOnTime();
+  loadTaskData();
 }
 
 async function loadUserData() {
@@ -9,9 +11,81 @@ async function loadUserData() {
     userFound(logInUser);
   } else {
     userInitials.innerHTML = "G";
-    greetingText.innerHTML = "Good Morning !";
-    greetingBox.innerHTML = "Good Morning !";
+    guestGreeting();
   }
+}
+
+function guestGreeting() {
+  let greetingText = document.getElementById("greetingText");
+  let greetingBox = document.getElementById("greetingBox");
+  let currentHour = new Date().getHours();
+  let greeting = "";
+  if (currentHour < 12) {
+    greeting = "Good Morning !";
+  } else if (currentHour < 16) {
+    greeting = "Good Day !";
+  } else if (currentHour < 18) {
+    greeting = "Good Afternoon !";
+  } else {
+    greeting = "Good Evening !";
+  }
+  greetingText.innerHTML = greeting;
+  greetingBox.innerHTML = greeting;
+}
+
+async function loadTaskData() {
+  let tasks = await getData((path = "task"));
+  let tasksArray = Object.values(tasks || {});
+  let toDoTasks = sortTasks(tasksArray, "todo");
+  let inProgressTasks = sortTasks(tasksArray, "inprogress");
+  let awaitFeedbackTasks = sortTasks(tasksArray, "feedback");
+  let doneTasks = sortTasks(tasksArray, "done");
+  showTaskCount(
+    tasksArray,
+    toDoTasks,
+    inProgressTasks,
+    awaitFeedbackTasks,
+    doneTasks
+  );
+}
+
+function sortTasks(tasksArray, status) {
+  let statusTasks = tasksArray.filter((task) => task.status === status);
+  return statusTasks;
+}
+
+function showTaskCount(
+  tasksArray,
+  toDoTasks,
+  inProgressTasks,
+  awaitFeedbackTasks,
+  doneTasks
+) {
+  document.getElementById("boardCount").innerHTML = tasksArray.length || "0";
+  document.getElementById("todoCount").innerHTML = toDoTasks.length || "0";
+  document.getElementById("progressCount").innerHTML =
+    inProgressTasks.length || "0";
+  document.getElementById("awaitingFeedbackCount").innerHTML =
+    awaitFeedbackTasks.length || "0";
+  document.getElementById("doneCount").innerHTML = doneTasks.length || "0";
+}
+
+function greetingBasedOnTime() {
+  let greetingText = document.getElementById("greetingResp");
+  let greetingBox = document.getElementById("greeting");
+  let currentHour = new Date().getHours();
+  let greeting = "";
+  if (currentHour < 12) {
+    greeting = "Good Morning, ";
+  } else if (currentHour < 16) {
+    greeting = "Good Day, ";
+  } else if (currentHour < 18) {
+    greeting = "Good Afternoon, ";
+  } else {
+    greeting = "Good Evening, ";
+  }
+  greetingText.innerHTML = greeting;
+  greetingBox.innerHTML = greeting;
 }
 
 function userFound(logInUser) {
