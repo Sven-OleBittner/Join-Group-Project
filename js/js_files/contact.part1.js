1e62cdb26111c3e23a49ef93c5707338c814cfe6
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2026-01-07 14:31:31 +0100

    chore: comment out BASE_URL declaration in contact.part1.js

--- js/js_files/contact.part1.js
+++ js/js_files/contact.part1.js
@@ -1,5 +1,5 @@
-const BASE_URL =
-    "https://join-1314-default-rtdb.europe-west1.firebasedatabase.app/";
+// const BASE_URL =
+//     "https://join-1314-default-rtdb.europe-west1.firebasedatabase.app/";
 let currentData = [];
 let selectedContactKey = null;
 
