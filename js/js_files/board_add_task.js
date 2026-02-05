b7a52a498353ff9c4d7455a713615dcda9bd2181
Sven-OleBittner <bittnersvenole@gmail.com>
2026-02-05 20:29:55 +0100

    feat: implement board initialization and task column visibility logic

--- js/js_files/board_add_task.js
+++ js/js_files/board_add_task.js
@@ -1,3 +1,28 @@
+function initBoardSite() {
+  checkColumns("toDoTaskList", "emptyToDo")
+  checkColumns("inProgressTaskList", "emptyInProgress")
+  checkColumns("awaitFeedbackTaskList", "emptyAwaitFeedback")
+  checkColumns("doneTaskList", "emptyDone")
+}
+
+function checkColumns(columnId, emptyColumnId) {
+  let column = document.getElementById(columnId);
+  let emptyColumn = document.getElementById(emptyColumnId);
+  if (column.innerHTML != "") {
+    emptyColumn.classList.add("d-none");
+  }
+}
+
+function chooseAddTaskSite(id) {
+  if (window.innerWidth > 945) {
+    generateAddTaskModal();
+  } else {
+    window.location.href = `add_task.html`;
+  }
+}
+
+// ?status=${getStatusByButtonId(id)}
+
 function generateAddTaskModal() {
   let dialog = document.getElementById("addTaskDialog");
   let dialogContent = document.getElementById("addTaskDialogContent");
