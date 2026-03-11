755d3a624b09942cb846d6a2e77040920c673adc
Sven-OleBittner <bittnersvenole@gmail.com>
2026-03-11 18:22:39 +0100

    feat(login): enhance error message display and adjust input styles for better user experience

--- js/js_files/login.js
+++ js/js_files/login.js
@@ -40,7 +40,12 @@ function showMsg() {
 }
 
 function userNotFound() {
-  window.location = "index.html?msg=User not Found";
+  let errorBox = document.getElementById("password-error");
+  let toggleIcon = document.getElementById("toggle-password");
+  let lockIcon = document.getElementById("lock-password");
+  errorBox.style.display = "block";
+  lockIcon.classList.toggle("toggle-password-icon");
+  toggleIcon.classList.toggle("toggle-password-icon-error");
 }
 
 async function loggingInUser(user) {
@@ -55,27 +60,5 @@ async function loginGuest() {
   await loggingInUser(guestUser);
   sessionStorage.setItem("justLoggedIn", "true");
   window.location.href = "summary.html";
-}
-
-/** Validiert das Email-Feld */
-function validateEmail() {
-  const email = document.getElementById("email");
-  const icon = document.getElementById("email-icon");
-  const isValid = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/i.test(email.value);
-  email.classList.toggle("error", !isValid);
-  return isValid;
-}
-
-function validatePassword() {
-    const passwordInput = document.getElementById("password");
-    const passwordError = document.getElementById("password-error");
-    const icon = document.getElementById("lock-password");
-    const toggleIcon = document.getElementById("toggle-password");
-    const isValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/m.test(passwordInput.value);
-    passwordInput.classList.toggle("error", !isValid);
-    icon.classList.toggle("input-icon", isValid);
-    icon.classList.toggle("input-icon-error", !isValid);
-    passwordError.style.display = isValid ? "none" : "block";
-    return isValid;
 }
 
